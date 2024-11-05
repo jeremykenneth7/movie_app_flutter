19596ab4f1e555b3ac5b4c3eba5df55bdb370321
jeremykenneth7 <jeremykenneth7@gmail.com>
2024-11-05 22:57:43 +0700

    add const di details

--- lib/details.dart
+++ lib/details.dart
@@ -21,18 +21,18 @@ class MovieDetailsScreen extends StatelessWidget {
             children: [
               Text(
                 movie.title,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
                   color: Colors.black87,
                 ),
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
                   boxShadow: [
-                    BoxShadow(
+                    const BoxShadow(
                       color: Colors.black26,
                       blurRadius: 10,
                       offset: Offset(0, 5),
@@ -47,8 +47,8 @@ class MovieDetailsScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(height: 20),
-              Text(
+              const SizedBox(height: 20),
+              const Text(
                 "Overview",
                 style: TextStyle(
                   fontSize: 22,
@@ -56,36 +56,36 @@ class MovieDetailsScreen extends StatelessWidget {
                   color: Colors.black87,
                 ),
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Text(
                 movie.overview,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 16,
                   color: Colors.black54,
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               Row(
                 children: [
-                  Icon(Icons.calendar_today, size: 20, color: Colors.black54),
-                  SizedBox(width: 5),
+                  const Icon(Icons.calendar_today, size: 20, color: Colors.black54),
+                  const SizedBox(width: 5),
                   Text(
                     "Release Date: ${DateFormat('d MMMM yyyy').format(DateTime.parse(movie.releaseDate))}",
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 16,
                       color: Colors.black54,
                     ),
                   ),
                 ],
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Row(
                 children: [
-                  Icon(Icons.star, size: 20, color: Colors.black54),
-                  SizedBox(width: 5),
+                  const Icon(Icons.star, size: 20, color: Colors.black54),
+                  const SizedBox(width: 5),
                   Text(
                     "Popularity: ${movie.popularity}",
-                    style: TextStyle(
+                    style: const TextStyle(
                       fontSize: 16,
                       color: Colors.black54,
                     ),
